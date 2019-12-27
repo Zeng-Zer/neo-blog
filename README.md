898ce687a3d1aad967b064b12f0426132bd792bb
David Zeng <david.zeng@epitech.eu>
2019-12-27 21:22:38 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
+### Blog-neo
+
+Blog-neo is blog based on neo blockchain
+
+This project covers basic smart contract interactions with react client api
+
+You can write post to the neo blockchain
+
 ### Running Blog-neo 
 
-Install dependencies `npm install`
-
-Build neo-one files `npx neo-one build`
-
-Start neo-one `npx neo-one start`
-
-Running React app `npm start`
-
-Run tests `npm test`
+* Install dependencies `npm install`
+* Build neo-one files `npx neo-one build`
+* Running React app `npm start` and access from http://localhost:3000/
+* Run tests `npm test`
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,14 +10,9 @@ class Navbar extends Component<NavbarProps> {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="#lol"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Blog based on NEO Blockchain
-        </a>
+        <div className="navbar-brand col-sm-3 col-md-2 mr-0">
+          Blog NEO Blockchain
+        </div>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
