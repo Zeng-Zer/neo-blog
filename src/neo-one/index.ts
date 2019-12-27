f8b2c632bd4632f815b90f229a9f875a4091dc0e
David Zeng <david.zeng@epitech.eu>
2019-12-27 20:53:55 +0100

    removed useless files

--- src/neo-one/index.ts
+++
@@ -1,9 +0,0 @@
-/* @hash a7fe8b7298f760f840dda3aeb7119727 */
-// tslint:disable
-/* eslint-disable */
-export * from './contracts';
-export * from './react';
-export * from './client';
-export * from './Blog/contract';
-export * from './Blog/types';
-export * from './Blog/abi';
